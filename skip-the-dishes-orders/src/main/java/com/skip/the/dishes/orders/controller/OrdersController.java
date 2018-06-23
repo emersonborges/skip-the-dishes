9d95afca4c216e0143c66fcb4e99e5dd69fdcc5a
Emerson Borges <emersonbrg@gmail.com>
2018-06-23 12:12:25 -0300

    Producing to kafka orders topic

--- skip-the-dishes-orders/src/main/java/com/skip/the/dishes/orders/controller/OrdersController.java
+++ skip-the-dishes-orders/src/main/java/com/skip/the/dishes/orders/controller/OrdersController.java
@@ -1,19 +1,28 @@
 package com.skip.the.dishes.orders.controller;
 
 import com.skip.the.dishes.orders.integration.ProductsApi;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class OrdersController {
     private ProductsApi productsApi;
+    private KafkaTemplate<String, String> kafkaTemplate;
+    private String topic;
 
-    public OrdersController(ProductsApi productsApi) {
+    public OrdersController(ProductsApi productsApi,
+                            KafkaTemplate<String, String> kafkaTemplate,
+                            @Value("${kafka.topic}") String topic) {
         this.productsApi = productsApi;
+        this.kafkaTemplate = kafkaTemplate;
+        this.topic = topic;
     }
 
     @PostMapping("/orders")
     public void createOrder() {
         productsApi.getById("1");
+        kafkaTemplate.send(topic, "OrderCreated");
     }
 }
