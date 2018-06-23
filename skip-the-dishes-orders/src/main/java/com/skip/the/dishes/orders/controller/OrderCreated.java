597cf7c81221dd2adfc000fda9336f1070f36fc6
Emerson Borges <emersonbrg@gmail.com>
2018-06-23 17:42:57 -0300

    Removing hard coded

---
+++ skip-the-dishes-orders/src/main/java/com/skip/the/dishes/orders/controller/OrderCreated.java
@@ -0,0 +1,16 @@
+package com.skip.the.dishes.orders.controller;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import static lombok.AccessLevel.PRIVATE;
+
+@Builder
+@NoArgsConstructor(access = PRIVATE)
+@AllArgsConstructor
+@Getter
+public class OrderCreated {
+    private String productName;
+}
--- skip-the-dishes-orders/src/main/java/com/skip/the/dishes/orders/controller/OrdersController.java
+++ skip-the-dishes-orders/src/main/java/com/skip/the/dishes/orders/controller/OrdersController.java
@@ -1,28 +1,46 @@
 package com.skip.the.dishes.orders.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.skip.the.dishes.orders.integration.ProductsApi;
+import com.skip.the.dishes.products.api.ProductResponse;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @RestController
 public class OrdersController {
     private ProductsApi productsApi;
     private KafkaTemplate<String, String> kafkaTemplate;
     private String topic;
+    private ObjectMapper objectMapper;
 
     public OrdersController(ProductsApi productsApi,
                             KafkaTemplate<String, String> kafkaTemplate,
-                            @Value("${kafka.topic}") String topic) {
+                            @Value("${kafka.topic}") String topic, ObjectMapper objectMapper) {
         this.productsApi = productsApi;
         this.kafkaTemplate = kafkaTemplate;
         this.topic = topic;
+        this.objectMapper = objectMapper;
     }
 
-    @PostMapping("/orders")
-    public void createOrder() {
-        productsApi.getById("e1d7acb3-f752-4e74-977b-92bd64f2d694");
-        kafkaTemplate.send(topic, "OrderCreated");
+    @PostMapping(value = "/orders", consumes = APPLICATION_JSON_VALUE)
+    public void createOrder(@RequestBody CreateOrderRequest createOrderRequest) {
+        //Save on relational database
+        ProductResponse product = productsApi.getById(createOrderRequest.getProductId());
+        OrderCreated event = OrderCreated.builder().productName(product.getName()).build();
+        kafkaTemplate.send(topic, toJson(event));
+    }
+
+    private String toJson(OrderCreated event) {
+        try {
+            return objectMapper.writeValueAsString(event);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException("Error to parse event");
+        }
     }
 }
