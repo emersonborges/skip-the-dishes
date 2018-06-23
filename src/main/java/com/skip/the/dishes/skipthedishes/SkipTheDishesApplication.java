2697f8e2127721d34107adcb7b05cf68a92fd8d0
Emerson Borges <emersonbrg@gmail.com>
2018-06-23 10:03:27 -0300

    Project created

---
+++ src/main/java/com/skip/the/dishes/skipthedishes/SkipTheDishesApplication.java
@@ -0,0 +1,12 @@
+package com.skip.the.dishes.skipthedishes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkipTheDishesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkipTheDishesApplication.class, args);
+	}
+}
